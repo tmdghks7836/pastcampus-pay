6edfa4059572e0c2d71b71bc0f070515b40c03d9
DESKTOP-VSDKPQV\tmdgh <tmdghks7836@naver.com>
2023-10-27 13:45:31 +0900

    ad

--- membership-service/src/main/java/com/fastcampuspay/membership/SwaggerConfig.java
+++ membership-service/src/main/java/com/fastcampuspay/membership/SwaggerConfig.java
@@ -14,7 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-
+    
     @Bean
     public Docket restAPI(){
         return new Docket(DocumentationType.SWAGGER_2)
